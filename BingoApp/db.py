b35fc49299309125a5f32a74bbc720cf585ac382
JLancaster25 <austin.lancaster7@gmail.com>
2026-01-17 19:52:21 -0600

    Delete BingoApp directory
    Signed-off-by: JLancaster25 <austin.lancaster7@gmail.com>

--- BingoApp/db.py
+++
@@ -1,9 +0,0 @@
-import os, psycopg2
-from psycopg2.extras import RealDictCursor
-
-def get_db():
-    return psycopg2.connect(
-        os.environ["DATABASE_URL"],
-        cursor_factory=RealDictCursor,
-        sslmode="require"
-    )
