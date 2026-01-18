09a1ffc42fa70c5b0de7d7f4a7ffe2a3bf87af18
JLancaster25 <austin.lancaster7@gmail.com>
2026-01-18 06:20:43 -0600

    Update bingo_engine.py
    Signed-off-by: JLancaster25 <austin.lancaster7@gmail.com>

--- BingoCommunity/repo/backend/bingo_engine.py
+++ BingoCommunity/repo/backend/bingo_engine.py
@@ -1,43 +1,66 @@
-# FILE: bingo_engine.py
-def __init__(self):
-  self.card = {
-  letter: random.sample(list(numbers), 5)
-  for letter, numbers in BINGO_LETTERS.items()
-  }
-  self.card["N"][2] = "X" # Free space
+import random
+
+BINGO_LETTERS = {
+    "B": range(1, 16),
+    "I": range(16, 31),
+    "N": range(31, 46),
+    "G": range(46, 61),
+    "O": range(61, 76),
+}
 
 
-def mark(self, number):
-  for letter in self.card:
-    for i, value in enumerate(self.card[letter]):
-      if value == number:
-      self.card[letter][i] = "X"
+class BingoCard:
+    def __init__(self):
+        self.card = {
+            letter: random.sample(list(numbers), 5)
+            for letter, numbers in BINGO_LETTERS.items()
+        }
+        # Free space
+        self.card["N"][2] = "X"
 
+    def mark(self, number):
+        for letter in self.card:
+            for i, value in enumerate(self.card[letter]):
+                if value == number:
+                    self.card[letter][i] = "X"
 
-def has_bingo(self):
-  grid = [self.card[l] for l in "BINGO"]
-  for row in zip(*grid):
-  if all(v == "X" for v in row): return True
-  for col in grid:
-  if all(v == "X" for v in col): return True
-  if all(grid[i][i] == "X" for i in range(5)): return True
-  if all(grid[i][4-i] == "X" for i in range(5)): return True
-  return False
+    def has_bingo(self):
+        grid = [self.card[l] for l in "BINGO"]
 
+        # Rows
+        for row in zip(*grid):
+            if all(v == "X" for v in row):
+                return True
 
+        # Columns
+        for col in grid:
+            if all(v == "X" for v in col):
+                return True
+
+        # Diagonals
+        if all(grid[i][i] == "X" for i in range(5)):
+            return True
+
+        if all(grid[i][4 - i] == "X" for i in range(5)):
+            return True
+
+        return False
 
 
 class BingoGame:
-  def __init__(self):
-    self.deck = [f"{l}{n}" for l, nums in BINGO_LETTERS.items() for n in nums]
-    random.shuffle(self.deck)
-    self.called = []
+    def __init__(self):
+        self.deck = [
+            f"{letter}{num}"
+            for letter, nums in BINGO_LETTERS.items()
+            for num in nums
+        ]
+        random.shuffle(self.deck)
+        self.called = []
 
+    def draw(self):
+        if not self.deck:
+            return None
 
-def draw(self):
-  if not self.deck: return None
-    call = self.deck.pop()
-    self.called.append(call)
-
-  return call
-
+        call = self.deck.pop()
+        self.called.append(call)
+        return call
